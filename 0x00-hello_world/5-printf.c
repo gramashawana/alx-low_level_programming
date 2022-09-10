ab79bea5c430f7ac9cdb96ba1132104a0e50e722
“gramashawana” <remilekun.mail@gmail.com>
2022-09-09 19:44:39 -0700

    hello, print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
-* main - print the string in the put function 
+* main - print the string in the put function
 *
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
@@ -9,6 +10,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0)
 	return (0);
 }
