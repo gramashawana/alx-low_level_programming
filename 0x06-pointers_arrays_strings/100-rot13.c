a18e393933d0cef112bbda866255489f9dec0cb3
“gramashawana” <remilekun.mail@gmail.com>
2022-09-21 20:42:42 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,34 +1,29 @@
 #include "main.h"
-#include <stdio.h>
- 
- /**
-  * rot 13 - ...
-  * @s: ...
-  *
-  * Return: ....
-  */
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
 char *rot13(char *s)
 {
-	int a = 0;
+	int i, j;
 
-	while (s[a])
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'z'))
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if ((s[a] >= 'm' && s[a] <= 'z') || (s[a] >= 'M' && s[a] <= 'z'))
+			if (s[i] == a[j])
 			{
-				s[a] -= 13;
+				s[i] = b[j];
 				break;
 			}
-
-			s[a] += 13;
-			break;
 		}
-
-		a++;
 	}
 
 	return (s);
 }
-
-
