f3ae6d02c8d31143e5ce0be16a72b4b69802f772
“gramashawana” <remilekun.mail@gmail.com>
2022-10-12 17:57:34 -0700

    dog

---
+++ 0x0E-structures_typedef/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code for alx students.
+ * 
+ * Return: Always 0.
+ */
+int main (void)
+{
+   struct dog my_dog;
+	
+   init_dog(&my_dog, "lenny", 3.5, "matty");
+   printf(my name is %s, an i am %.if :) - woof!\n", my_dog.name, my_dog.age);
+   return (0);
+}
