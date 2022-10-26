aa3754e62f75374da48b957793ccdf4e9000fc63
“gramashawana” <remilekun.mail@gmail.com>
2022-10-25 18:59:55 -0700

    latest

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,12 +11,12 @@ size_t list_len(const list_t *h)
 {
 		size_t n = 0;
 
-			while (h)
-					{
-						n++;
-						h = h->next;
-					}
-				return (n);
+		while (h)
+		{
+			n++;
+			h = h->next;
+		}
+		return (n);
 }
 
 
